2fafeac63c511e1510171e87dca9e56d6ea2ad36
pefacel <pefacel@gmail.com>
2021-01-14 04:32:05 -0300

    Creación de clientes siendo consumidos desde un archivo JSON

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,12 @@
+import { Cliente } from './cliente';
+
+
+export const CLIENTES: Cliente[] = [
+    { id: 1, nombreDuenio: 'MrMagoo', apellidoDuenio: 'Mag', creatAt: '2017-12-11', email: 'pefacel@gmail.com' },
+    { id: 2, nombreDuenio: 'Matias', apellidoDuenio: 'Lara', creatAt: '2017-12-11', email: 'pefacel@gmail.com' },
+    { id: 3, nombreDuenio: 'Rodrigo', apellidoDuenio: 'Alfonsín', creatAt: '2017-12-11', email: 'pefacel@gmail.com' },
+    { id: 4, nombreDuenio: 'Carlos', apellidoDuenio: 'Adelio', creatAt: '2017-12-11', email: 'pefacel@gmail.com' },
+    { id: 5, nombreDuenio: 'Dino', apellidoDuenio: 'Fuenzalida', creatAt: '2017-12-11', email: 'pefacel@gmail.com' }
+
+
+  ];
